21db08d1deaaa9e9612f86a205ff6f102f78cb8c
dmouchene-devops <71235789+dmouchene-devops@users.noreply.github.com>
2021-10-17 23:20:21 +0200

    Read sales.csv as RDD of String

--- spark-learning/src/main/java/fr/htc/spark/main/RunIt.java
+++ spark-learning/src/main/java/fr/htc/spark/main/RunIt.java
@@ -1,23 +1,24 @@
 package fr.htc.spark.main;
 
-import org.apache.spark.SparkContext;
+import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
-import org.apache.spark.sql.SparkSession;
 
 import fr.htc.spark.config.SparkConfig;
 
 public class RunIt {
 
+	/**
+	 * Question N° 2 : Charger le fichier des ventes (sales.csv) dans une RDD de
+	 * type String
+	 * 
+	 * @param args
+	 */
 	public static void main(String[] args) {
 
-		SparkSession sparkSession = SparkConfig.getSparkSession();
-		System.out.println(sparkSession.toString());
-
-		SparkContext sparkContext = SparkConfig.getSparkContext();
-		System.out.println(sparkContext);
-
-		JavaSparkContext javaSparkContext = SparkConfig.getJavaSparkContext();
-		System.out.println(javaSparkContext);
+		JavaSparkContext jsc = SparkConfig.getJavaSparkContext();
+		JavaRDD<String> salesAsStringRDD = jsc.textFile("data/sales.csv");
+		// Afficher 10 éléments du RDD
+		salesAsStringRDD.take(10).stream().forEach(line -> System.out.println(line));
 
 	}
 
