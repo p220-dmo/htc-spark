faf6120ce6b6206c455c52101aa1f6542e63e42d
dmouchene-devops <71235789+dmouchene-devops@users.noreply.github.com>
2021-10-17 23:55:36 +0200

    CA by Store

--- spark-learning/src/main/java/fr/htc/spark/main/RunIt.java
+++ spark-learning/src/main/java/fr/htc/spark/main/RunIt.java
@@ -1,16 +1,19 @@
 package fr.htc.spark.main;
 
+import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.api.java.function.Function2;
+import org.apache.spark.api.java.function.PairFunction;
 
 import fr.htc.spark.beans.Sale;
 import fr.htc.spark.config.SparkConfig;
+import scala.Tuple2;
 
 public class RunIt {
 
 	/**
-	 * Exercice 3: Charger le fichier des ventes (sales.csv) dans une RDD de type
-	 * Sale. La classe Sale est aussi à développer type String
+	 * Exercice 4: Calculer le chiffre d'affaire par magasin
 	 * 
 	 * @param args
 	 */
@@ -19,16 +22,20 @@ public class RunIt {
 		JavaSparkContext jsc = SparkConfig.getJavaSparkContext();
 		JavaRDD<String> salesAsStringRDD = jsc.textFile("data/sales.csv");
 
+		JavaRDD<Sale> salesAsObjects = salesAsStringRDD.map(csvLine -> Sale.parse(csvLine, ";"));
 
-		// Charger le fichier comme à l'exercice 2 et mapper chaque ligne vers un objet
-		// Sale qu'on aura créé !!!
+		// On peut le faire avec reduceByKey ou groupByKey. Cependant, il faut
+		// privilègier reduceByKey, car un calcul intermédiaire se fait au niveau de
+		// chaque partition avant d'envoyer les résultats sur le réseau. Ce qui fait
+		// qu'on n'envoie qu'un tuple par clé et par partition. Conséquence, moins de
+		// trafic réseau !!!
 		
-		JavaRDD<Sale> salesAsObjects = salesAsStringRDD
-				 .map(csvLine -> Sale.parse(csvLine, ";"));
-		
-		//Afficher les 10 premier objets Sale
-		
-		salesAsObjects.take(10).forEach(sale -> System.out.println(sale));
+		JavaPairRDD<Long, Double> storeCA = salesAsObjects.mapToPair(
+				(PairFunction<Sale, Long, Double>) sale -> new Tuple2<Long, Double>(sale.getStoreId(), sale.getCa()))
+				.reduceByKey((Function2<Double, Double, Double>) (ca1, ca2) -> ca1 + ca2);
+
+		storeCA.collectAsMap().forEach((storeId, caByStore) -> System.out
+				.println("Magasin : " + storeId + " a un chiffre d'affaires : " + caByStore));
 
 	}
 
