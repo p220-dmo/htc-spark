c24c612027d4ddb52dea0be19beccd8e6388bed7
dmouchene-devops <71235789+dmouchene-devops@users.noreply.github.com>
2021-11-10 17:46:37 +0100

    Exercice 7 : real Solution

--- spark-learning/src/main/java/fr/htc/spark/main/RunIt.java
+++ spark-learning/src/main/java/fr/htc/spark/main/RunIt.java
@@ -18,7 +18,8 @@ import scala.reflect.ClassTag$;
 public class RunIt {
 
 	/**
-	 * Exercice 7: Comparer les ventes (en termes de CA) entre les premiers trimestres (Q1) de 1997 et 1998
+	 * Exercice 7: Comparer les ventes (en termes de CA) entre les premiers
+	 * trimestres (Q1) de 1997 et 1998
 	 * 
 	 * @param args
 	 */
@@ -27,18 +28,26 @@ public class RunIt {
 		JavaSparkContext jsc = SparkConfig.getJavaSparkContext();
 		JavaRDD<String> salesAsStringRDD = jsc.textFile("data/sales.csv");
 		JavaRDD<Sale> salesAsObjects = salesAsStringRDD.map(csvLine -> Sale.parse(csvLine, ";"));
-		/*$$$$$   /$$              /$$$$$$$$ /$$$$$$  /$$   /$$ /$$$$$$$$    /$$$$$$$   /$$$$$$   /$$$$$$   /$$$$$$  /$$$$$$$$ /$$$$$$$ 
-		|_  $$_/  | $$             | $$_____//$$__  $$| $$  | $$|__  $$__/   | $$__  $$ /$$__  $$ /$$__  $$ /$$__  $$| $$_____/| $$__  $$
-		  | $$    | $$             | $$     | $$  \ $$| $$  | $$   | $$      | $$  \ $$| $$  \ $$| $$  \__/| $$  \__/| $$      | $$  \ $$
-		  | $$    | $$             | $$$$$  | $$$$$$$$| $$  | $$   | $$      | $$$$$$$ | $$  | $$|  $$$$$$ |  $$$$$$ | $$$$$   | $$$$$$$/
-		  | $$    | $$             | $$__/  | $$__  $$| $$  | $$   | $$      | $$__  $$| $$  | $$ \____  $$ \____  $$| $$__/   | $$__  $$
-		  | $$    | $$             | $$     | $$  | $$| $$  | $$   | $$      | $$  \ $$| $$  | $$ /$$  \ $$ /$$  \ $$| $$      | $$  \ $$
-		 /$$$$$$  | $$$$$$$$       | $$     | $$  | $$|  $$$$$$/   | $$      | $$$$$$$/|  $$$$$$/|  $$$$$$/|  $$$$$$/| $$$$$$$$| $$  | $$
-		|______/  |________/       |__/     |__/  |__/ \______/    |__/      |_______/  \______/  \______/  \______/ |________/|__/  |_*/
-		
-	                                                                                                                                              
-	
-		
+
+// Clé=TimeId et Valeur=Année
+		Map<Integer, Integer> quarterTimeId = jsc.textFile("data/time.csv").map(time -> TimeByDay.parse(time))
+				.filter(time -> time.getQuarter().equals("Q1"))
+				.mapToPair(timeByDay -> new Tuple2<Integer, Integer>(timeByDay.getTimeId(), timeByDay.getYear()))
+				.collectAsMap();
+
+//Broadcast 
+		Broadcast<Map<Integer, Integer>> filteredTimeIds = SparkConfig.getSparkContext().broadcast(quarterTimeId,
+				ClassTag$.MODULE$.apply(Map.class));
+
+		JavaPairRDD<Integer, Double> yearCAQuarter = salesAsObjects
+				.filter(sale -> filteredTimeIds.value().containsKey(sale.getTimeId()))
+				.mapToPair(
+						sale -> new Tuple2<>(filteredTimeIds.value().getOrDefault(sale.getTimeId(), -1), sale.getCa()))
+				.reduceByKey((x, y) -> x + y);
+
+		yearCAQuarter.collectAsMap()
+				.forEach((yearKey, caByYear) -> System.out.println("CA Q1 de l'année " + yearKey + " : " + caByYear));
+
 	}
 
 }
