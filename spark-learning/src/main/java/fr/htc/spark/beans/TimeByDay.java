c18762e3f7ba2885f3b43d622c9ff22e27f21b4b
dmouchene-devops <71235789+dmouchene-devops@users.noreply.github.com>
2021-11-10 17:28:42 +0100

    Exercice 7: Comparer les ventes (en termes de CA) entre les premiers trimestres (Q1) de 1997 et 1998

---
+++ spark-learning/src/main/java/fr/htc/spark/beans/TimeByDay.java
@@ -0,0 +1,44 @@
+package fr.htc.spark.beans;
+
+import java.io.Serializable;
+
+public class TimeByDay implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	private Integer timeId;
+	private Integer year;
+	private String quarter;
+	public Integer getTimeId() {
+		return timeId;
+	}
+	public void setTimeId(Integer timeId) {
+		this.timeId = timeId;
+	}
+	public Integer getYear() {
+		return year;
+	}
+	public void setYear(Integer year) {
+		this.year = year;
+	}
+	public String getQuarter() {
+		return quarter;
+	}
+	public void setQuarter(String quarter) {
+		this.quarter = quarter;
+	}
+	
+	
+	public static TimeByDay parse(String line) {
+		String[] columns = line.split(";");
+		TimeByDay timeByDate = new TimeByDay();
+		
+		timeByDate.setTimeId(Integer.parseInt(columns[0]));
+		timeByDate.setYear(Integer.parseInt(columns[4]));
+		timeByDate.setQuarter(columns[8]);
+		
+		return timeByDate;
+		
+		
+	}
+
+}
--- spark-learning/src/main/java/fr/htc/spark/main/RunIt.java
+++ spark-learning/src/main/java/fr/htc/spark/main/RunIt.java
@@ -1,14 +1,16 @@
 package fr.htc.spark.main;
 
 import java.util.Map;
+import java.util.function.Function;
 
 import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.api.java.function.PairFunction;
 import org.apache.spark.broadcast.Broadcast;
 
 import fr.htc.spark.beans.Sale;
-import fr.htc.spark.beans.Store;
+import fr.htc.spark.beans.TimeByDay;
 import fr.htc.spark.config.SparkConfig;
 import scala.Tuple2;
 import scala.reflect.ClassTag$;
@@ -16,7 +18,7 @@ import scala.reflect.ClassTag$;
 public class RunIt {
 
 	/**
-	 * Exercice 6: Calculer le chiffre d'affaire par région.
+	 * Exercice 7: Comparer les ventes (en termes de CA) entre les premiers trimestres (Q1) de 1997 et 1998
 	 * 
 	 * @param args
 	 */
@@ -25,30 +27,18 @@ public class RunIt {
 		JavaSparkContext jsc = SparkConfig.getJavaSparkContext();
 		JavaRDD<String> salesAsStringRDD = jsc.textFile("data/sales.csv");
 		JavaRDD<Sale> salesAsObjects = salesAsStringRDD.map(csvLine -> Sale.parse(csvLine, ";"));
-
-		// Lecture du fichier store à broadcaster er reconversion en Map <StoreId, RegionId> (fichier très petit)
-		Map<Integer, Integer> storeRegionMap = SparkConfig.getJavaSparkContext().textFile("data/store.csv")
-				.map(storeAsCsv -> Store.parse(storeAsCsv))
-				.mapToPair(store -> new Tuple2<>(store.getId(), store.getRegionId())).collectAsMap();
-
-		// Version Eclatée : Lecture du fichier store à broadcaster er reconversion en Map <StoreId, RegionId> (fichier très petit)
-//		Map<Integer, Integer> storeRegionMapRdd = null;
-//		JavaRDD<String> storeStringRDD = SparkConfig.getJavaSparkContext().textFile("data/store.csv");
-//		JavaRDD<Store> storeRDD = storeStringRDD.map(storeAsCsv -> Store.parse(storeAsCsv));
-//		JavaPairRDD<Integer, Integer> toto = storeRDD.mapToPair(store ->  new Tuple2<>(store.getId(), store.getRegionId()));
-
-		// Broadcater la Map à travers les Workers
-		Broadcast<Map<Integer, Integer>> storeRegionMapBrdct = SparkConfig.getSparkContext().broadcast(storeRegionMap,
-				ClassTag$.MODULE$.apply(Map.class));
-
-		// Faire un Map-side Join
-		JavaPairRDD<Integer, Double> caByRegion = salesAsObjects
-				.mapToPair(sale -> new Tuple2<>(storeRegionMapBrdct.value().getOrDefault(sale.getStoreId(), -1),
-						sale.getUnitSales() * sale.getStoreSales()))
-				.reduceByKey((a, b) -> a + b);
-
-		caByRegion.collectAsMap().forEach((regionIdKey, caByRegionValue) -> System.out
-				.println("Region : " + regionIdKey + " avec un CA : " + caByRegionValue));
+		/*$$$$$   /$$              /$$$$$$$$ /$$$$$$  /$$   /$$ /$$$$$$$$    /$$$$$$$   /$$$$$$   /$$$$$$   /$$$$$$  /$$$$$$$$ /$$$$$$$ 
+		|_  $$_/  | $$             | $$_____//$$__  $$| $$  | $$|__  $$__/   | $$__  $$ /$$__  $$ /$$__  $$ /$$__  $$| $$_____/| $$__  $$
+		  | $$    | $$             | $$     | $$  \ $$| $$  | $$   | $$      | $$  \ $$| $$  \ $$| $$  \__/| $$  \__/| $$      | $$  \ $$
+		  | $$    | $$             | $$$$$  | $$$$$$$$| $$  | $$   | $$      | $$$$$$$ | $$  | $$|  $$$$$$ |  $$$$$$ | $$$$$   | $$$$$$$/
+		  | $$    | $$             | $$__/  | $$__  $$| $$  | $$   | $$      | $$__  $$| $$  | $$ \____  $$ \____  $$| $$__/   | $$__  $$
+		  | $$    | $$             | $$     | $$  | $$| $$  | $$   | $$      | $$  \ $$| $$  | $$ /$$  \ $$ /$$  \ $$| $$      | $$  \ $$
+		 /$$$$$$  | $$$$$$$$       | $$     | $$  | $$|  $$$$$$/   | $$      | $$$$$$$/|  $$$$$$/|  $$$$$$/|  $$$$$$/| $$$$$$$$| $$  | $$
+		|______/  |________/       |__/     |__/  |__/ \______/    |__/      |_______/  \______/  \______/  \______/ |________/|__/  |_*/
+		
+	                                                                                                                                              
+	
+		
 	}
 
 }
