75b006acce84eb127bf1bf012398ca119d91eb90
dmouchene-devops <71235789+dmouchene-devops@users.noreply.github.com>
2021-11-10 15:59:54 +0100

    somme modifications & optimisation + build Map step by step (Commented)

--- spark-learning/src/main/java/fr/htc/spark/main/RunIt.java
+++ spark-learning/src/main/java/fr/htc/spark/main/RunIt.java
@@ -5,8 +5,6 @@ import java.util.Map;
 import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
-import org.apache.spark.api.java.function.Function2;
-import org.apache.spark.api.java.function.PairFunction;
 import org.apache.spark.broadcast.Broadcast;
 
 import fr.htc.spark.beans.Sale;
@@ -28,23 +26,29 @@ public class RunIt {
 		JavaRDD<String> salesAsStringRDD = jsc.textFile("data/sales.csv");
 		JavaRDD<Sale> salesAsObjects = salesAsStringRDD.map(csvLine -> Sale.parse(csvLine, ";"));
 
-		// Lecture du fichier store à broadcaster (fichier très petit)
-		Map<Integer, Integer> storeRegionMapRdd = SparkConfig.getJavaSparkContext().textFile("data/store.csv")
-				.mapToPair((PairFunction<String, Integer, Integer>) s -> {
-					Store store = Store.parse(s);
-					return new Tuple2<>(store.getId(), store.getRegionId());
-				}).collectAsMap();
-		Broadcast<Map<Integer, Integer>> storeRegionMap = SparkConfig.getSparkContext().broadcast(storeRegionMapRdd,
+		// Lecture du fichier store à broadcaster er reconversion en Map <StoreId, RegionId> (fichier très petit)
+		Map<Integer, Integer> storeRegionMap = SparkConfig.getJavaSparkContext().textFile("data/store.csv")
+				.map(storeAsCsv -> Store.parse(storeAsCsv))
+				.mapToPair(store -> new Tuple2<>(store.getId(), store.getRegionId())).collectAsMap();
+
+		// Version Eclatée : Lecture du fichier store à broadcaster er reconversion en Map <StoreId, RegionId> (fichier très petit)
+//		Map<Integer, Integer> storeRegionMapRdd = null;
+//		JavaRDD<String> storeStringRDD = SparkConfig.getJavaSparkContext().textFile("data/store.csv");
+//		JavaRDD<Store> storeRDD = storeStringRDD.map(storeAsCsv -> Store.parse(storeAsCsv));
+//		JavaPairRDD<Integer, Integer> toto = storeRDD.mapToPair(store ->  new Tuple2<>(store.getId(), store.getRegionId()));
+
+		// Broadcater la Map à travers les Workers
+		Broadcast<Map<Integer, Integer>> storeRegionMapBrdct = SparkConfig.getSparkContext().broadcast(storeRegionMap,
 				ClassTag$.MODULE$.apply(Map.class));
 
 		// Faire un Map-side Join
 		JavaPairRDD<Integer, Double> caByRegion = salesAsObjects
-				.mapToPair(sale -> new Tuple2<>(storeRegionMap.value().getOrDefault(sale.getStoreId(), -1),
+				.mapToPair(sale -> new Tuple2<>(storeRegionMapBrdct.value().getOrDefault(sale.getStoreId(), -1),
 						sale.getUnitSales() * sale.getStoreSales()))
-				.reduceByKey((Function2<Double, Double, Double>) (a, b) -> a + b);
+				.reduceByKey((a, b) -> a + b);
 
-		caByRegion.collectAsMap().forEach((k, v) -> System.out.println("Region : " + k + " avec un CA : " + v));
-
+		caByRegion.collectAsMap().forEach((regionIdKey, caByRegionValue) -> System.out
+				.println("Region : " + regionIdKey + " avec un CA : " + caByRegionValue));
 	}
 
 }
